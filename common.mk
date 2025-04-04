13600aaf0d63e5b41b8528655ad9ee63403fbd15
Pranav Vashi <neobuddy89@gmail.com>
2025-04-04 12:02:39 +0530

    sm8850-common: Build DeviceAsWebcam service
    * We disabled this by default on crdroid.
    
    Change-Id: Idd5b6fe7e2e0e1baa2156af637a00b0d3506ba6a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -97,6 +97,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti.recovery
 
 # Camera
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider-V3-ndk.vendor
 
