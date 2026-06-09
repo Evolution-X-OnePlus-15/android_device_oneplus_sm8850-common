17ed64b0fd06e12e77f2a040c676c81adf60d92e
Jonas Salo <salomannen1234@gmail.com>
2026-06-09 10:01:18 +0200

    sm8850-common: Add DT2W workaround

--- common.mk
+++ common.mk
@@ -698,12 +698,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
-# Inherit from the proprietary files makefile.
-$(call inherit-product, vendor/oneplus/sm8850-common/sm8850-common-vendor.mk)
-
 # DT2W
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/touchpanel.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/touchpanel.kl
+    device/oneplus/sm8850-common/keylayout/touchpanel.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/touchpanel.kl
+
+# Inherit from the proprietary files makefile.
+$(call inherit-product, vendor/oneplus/sm8850-common/sm8850-common-vendor.mk)
 
 # RKPD
 PRODUCT_PRODUCT_PROPERTIES += \
