bfa3bbf2b0a820d80f1831a86549a46dbc04a7f3
Marco Veldhuizen <marco_veldhuizen@hotmail.com>
2026-06-20 21:21:05 +0200

    Revert "sm8850-common: Build oplus als"
    This reverts commit f00bd3e71069a63dd7480e3050614670d4508d02.

--- common.mk
+++ common.mk
@@ -544,9 +544,7 @@ endif
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.oplus-multihal \
-    vendor.lineage.oplus_als.service \
-    sensors.dynamic_sensor_hal \
-    sensors.qsh_wrapper
+    sensors.dynamic_sensor_hal
     
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
